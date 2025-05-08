5291548e36eaee15e376ec8aeace7aba6038c150
Asli2024 <ashleyaden4u@hotmai.com>
2025-05-08 22:57:28 +0100

    Test

--- main.tf
+++ main.tf
@@ -2,6 +2,15 @@ resource "aws_instance" "test" {
   ami           = data.aws_ami.latest.id
   instance_type = var.instance_type
 
+  metadata_options {
+    http_tokens   = "required"
+    http_endpoint = "enabled"
+  }
+
+  root_block_device {
+    encrypted = true
+  }
+
   tags = {
     Name = "ExampleInstance"
   }
